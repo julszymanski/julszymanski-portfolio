049f86f3e94e2f9d1c617bbf834c6c2010ca211d
julszymanski <85003886+julszymanski@users.noreply.github.com>
2021-12-03 18:27:12 -0500

    portfolio restructure

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,67 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import {useDispatch} from 'react-redux';
-import {useSelector} from 'react-redux';
-import thumbsup from '../images/thumbsup.jpeg';
 import Images from '../data/Products';
 import fizzyfizzy from '../images/fizzyfizzy.jpeg';
-
-// ,counterValue1,incrementValue1,counterValue2,incrementValue2
-
-function MyProjects(props) {
-    const [projectData,setProjectData] = useState([]);
-    const dispatch = useDispatch();
-    const likes = useSelector(state => state.counterValue)
-
-    const handleStore = () =>{
-
-        dispatch({type:'LIKED'})
-    }
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 
-    const fetchProjects = () =>{
-        return axios.get("https://mocki.io/v1/fe6cb82c-d3e5-4973-9c02-9f8d26298c48")
-        .then(projects=>projects.data);
-    }
-
-    useEffect(()=>{
-        fetchProjects().then(projectResponse=>{
-            setProjectData(projectResponse['projects']);
-        })
-    },[])
-
+export default function MyProjects() {
     return (
-        <div className="container">
-            <table className="table" style={{marginLeft:'5em'}}>
-                <thead>
-                    <tr>
-                        <th style={{color: "#21ce99"}}>Project Name</th>
-                        <th style={{color: "#21ce99"}}>Date</th>
-                        <th style={{color: "#21ce99"}}>Description</th>
-                        {/* <th style={{color: "#21ce99"}}>Quick View</th> */}
-                        <th style={{color: "#21ce99"}}>URL</th>
-                        {/* <th style={{color: "#21ce99"}}>Likes</th> */}
-                    </tr>
-                </thead>
-                {
-                    projectData.map((project,index)=>(
-                        <tr>
-                            <td>{project.project_name}</td>
-                            <td>{project.date}</td>
-                            <td>{project.description}</td>
-                            {/* <img class="apiImage" src={project.quick_view} alt={project.quick_view}/> */}
-                            <td><button onClick={(e) => {e.preventDefault();window.location.href=project.url;}}>Click to View</button></td>
-                            {/* <button><img class="thumbsup" src={thumbsup} alt="heartbutton" onClick={handleStore}/></button><h2>{likes}</h2> */}
-
-                        </tr>
-                    ))
-                }
-            </table>
-            <button><img class="thumbsup" src={thumbsup} alt="heartbutton" onClick={handleStore}/></button>
-            <h2>{likes}</h2>
-        </div>
+        <Table size="sm">
+  <thead>
+    <tr>
+      <th scope="col">Project Name</th>
+      <th scope="col">Date</th>
+      <th scope="col">Description</th>
+      <th scope="col">URL</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">My Covid Safe Travels App</th>
+      <td>September 2021</td>
+      <td>Front-End Project demonstrating knowledge of HTML, CSS, Javascript, AJAX, APIs. Quantitative API data rendered using Chart.js </td>
+      <td><Button href="https://safetravel-s.netlify.app/">Click to View</Button></td>
+    </tr>
+    <tr>
+      <th scope="row">Fizzy Fizzy BathBombs</th>
+      <td>October 2021</td>
+      <td>Full-Stack Application utilizing Node, Express, Sequelize, PostgreSQL, bcrypt, Express ES6 Template Engine</td>
+      <td><Button href="https://fizzyfizzybathbombs.herokuapp.com/">Click to View</Button></td>
+    </tr>
+    <tr>
+      <th scope="row">Capstone Project</th>
+      <td>December 2021</td>
+      <td>Full-Stack Application using React</td>
+      <td><Button href="https://media-watch-list.herokuapp.com/">Click to View</Button></td>
+    </tr>
+  </tbody>
+</Table>
     );
 }
-
-export default MyProjects
